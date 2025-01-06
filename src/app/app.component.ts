691b1e9534e090f02aa5fcc5d0abe1b0a8f746ae
Saha <kishore.saha@siemens.com>
2025-01-06 19:39:17 +0530

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,21 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+import { AppTasksComponent } from './app-tasks/app-tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, AppTasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log('Selected user id: ' + id);
+
+  }
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,75 +1,41 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, computed, Input, input, Output, EventEmitter, output } from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  // imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
 
-  //  selectedUser = DUMMY_USERS[randomIndex];
-   selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-   //computed property/value using signal
-   imagePath = computed(()=> 'assets/users/' + this.selectedUser().avatar);
+  //decorator approach
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
-   //getter
-  //  get imagePath() {
-  //     return 'assets/users/' + this.selectedUser.avatar
-  //  }
+  //output property approach, custom event, it's not a signal unlike input, this is new approach
+  // select = output<string>();
 
+  //output property approach, custom event
+  @Output() select = new EventEmitter<string>();
 
+  // input function approach, single bases input
+  // avatar = input.required<string>();
+  // name = input.required<string>();
 
-   //user event handler method
+  //computed approach
+  // imagePath = computed(() => 'assets/users/' + this.avatar());
+
+  //get function approach
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
     onSelectUser() {
-        console.log('User clicked');
-        const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-        //state change
-        // this.selectedUser = DUMMY_USERS[randomIndex];
-
-        //signal value change by set method
-        this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.id);
     }
 
-  // constructor() {
-  //   console.log('UserComponent constructor');
-  // }
-
-  // ngOnInit() {
-  //   console.log('UserComponent ngOnInit');
-  // }
-
-  // ngOnDestroy() {
-  //   console.log('UserComponent ngOnDestroy');
-  // }
-
-  // ngOnChanges() {
-  //   console.log('UserComponent ngOnChanges');
-  // }
-
-  // ngDoCheck() {
-  //   console.log('UserComponent ngDoCheck');
-  // }
-
-  // ngAfterContentInit() {
-  //   console.log('UserComponent ngAfterContentInit');
-  // }
-
-  // ngAfterContentChecked() {
-  //   console.log('UserComponent ngAfterContentChecked');
-  // }
-
-  // ngAfterViewInit() {
-  //   console.log('UserComponent ngAfterViewInit');
-  // }
-
-  // ngAfterViewChecked() {
-  //   console.log('UserComponent ngAfterViewChecked');
-  // }
 
 }
