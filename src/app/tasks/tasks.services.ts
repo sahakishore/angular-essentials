50ab4e909a437327123a5fde14de319915d6fee5
Saha <kishore.saha@siemens.com>
2025-01-16 20:22:28 +0530

    Updated

--- src/app/tasks/tasks.services.ts
+++ src/app/tasks/tasks.services.ts
@@ -31,6 +31,14 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if(tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userIdParam: string) {
     return this.tasks.filter((task) => task.userId === userIdParam);
   }
@@ -43,9 +51,16 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: '2025-12-31',
     });
+
+    this.saveTasks();
   }
 
   removeTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
