d091cf4e31aea5a71e275f2ed97125e5c11ae2ae
Saha <kishore.saha@siemens.com>
2025-01-16 19:04:13 +0530

    finished app

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,13 @@
 import { Component, computed, Input, input, Output, EventEmitter, output } from '@angular/core';
 
 import { type IUser } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
+  imports: [CardComponent]
 })
 
 
